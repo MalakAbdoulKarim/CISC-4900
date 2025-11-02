620b037158d8a6dc68b96ab4c21523f2fcf04a56
Malak Y <164457476+MalakAbdoulKarim@users.noreply.github.com>
2025-11-02 15:30:53 -0500

    Update smart_lab_assistant_prototype.py

--- smart_lab_assistant_prototype.py
+++ smart_lab_assistant_prototype.py
@@ -6,7 +6,9 @@ print("Welcome to Smart Lab Assistant")
 print("What issue are you facing?")
 print("1. Login Issue")
 print("2. MFA questions")
-print("3. Software Crash")
+print("3. Email (BC Email / Outlook) login & password reset")
+Print("4.Wi-Fi access issues")
+
 
 # This block captures the user's choice from the menu.
 # Input: A string representing a menu number (e.g., "1", "2", or "3").
@@ -36,14 +38,20 @@ elif choice == "2":
     print("- If issues persist, reset your MFA or contact IT support")
 
 
-# Conditional block for "3. Software Crash".
-# Executes if the input 'choice' is exactly "3".
-# Output: Prints three specific troubleshooting steps for software crashes.
 elif choice == "3":
-    print("Troubleshooting Software:")
-    print("- Restart the program")
-    print("- Check if your file format is supported")
-    print("- Update to the latest version")
+    print("Troubleshooting Email login:")
+    print("- Make sure the user is entering the correct CUNY Login format(firstname.lastname##@login.cuny.edu)")
+    print("- Reset password via CUNY Login page")
+    print("- Wait 30 minutes OR contact campus IT for account unlock")
+
+
+elif choice == "4":
+    print("Troubleshooting Wi-Fi access issues:")
+    print("- Correct eduroam Login(firstname.lastname##@login.cuny.edu)")
+    print("- Forget & Re-Add Network")
+    print("- Wait 30 minutes OR contact campus IT for account unlock")
+
+
 
 # Fallback block.
 # Executes if the input 'choice' does not match any of the defined options ("1", "2", or "3").
