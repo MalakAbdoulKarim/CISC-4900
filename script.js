653b5c3dcef3bcdbf218280cbb61cb08b125412e
evelynk04 <159092826+evelynk04@users.noreply.github.com>
2025-11-05 13:43:21 -0500

    Enhance outgoing message handling and bot response
    Refactor message handling to include user data and simulate bot response.

--- script.js
+++ script.js
@@ -1,14 +1,52 @@
+const chatBody = document.querySelector(".chat-body"); 
 const messageInput = document.querySelector(".message-input"); 
+const sendMessageButton = document.querySelector("#send-message"); 
 
-const handleOutgoingMessage = (userMessage) => {
-    const messageContent = `<div class="message-text">${}</div>`;
-
+const userData = {
+    message : null
 }
 
+
+const createMessageElement = (content, classes) => {
+    const div = document.createElement("div");
+    div.classList.add("message", classes); 
+    div.innerHTML = content; 
+    return div;
+}
+
+const handleOutgoingMessage = (e) => {
+    e.preventDefault();
+    userData.message = messageInput.value.trim();
+    messageInput.value = "";
+
+    // Create and append outgoing message element   
+    const messageContent = `<div class="message-text">${userData.message}</div>`;
+
+    const outgoingMessageDiv = createMessageElement( messageContent,"user-message");
+    outgoingMessageDiv.querySelector(".message-text").textContent = userData.message;
+    chatBody.appendChild(outgoingMessageDiv);
+
+    // Simulate bot response after a delay 
+    setTimeout(() => {
+        const messageContent = `<div class="message bot-message thinking">
+                <div class="message-text">
+                    <div class ="thinking-dots">
+                        <div class="dot"></div>
+                        <div class="dot"></div>
+                        <div class="dot"></div>`;
+
+    const incomingMessageDiv = createMessageElement( messageContent,"bot-message");
+    chatBody.appendChild(incomingMessageDiv);
+    }, 600);
+}
+
+// Enter key press for sending message 
 messageInput.addEventListener("keydown", (e) => {
     const userMessage = e.target.value.trim();
     if(e.key === "Enter" && userMessage) {
-        console.log("User message:", userMessage);
-        handleOutgoingMessage(userMessage)
+        handleOutgoingMessage(e); 
     }
 }); 
+
+sendMessageButton.addEventListener("click", (e) => handleOutgoingMessage(e))
+
