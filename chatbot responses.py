f904a38866f7ed8f4cb7388a0bc28fee6999cb56
evelynk04 <159092826+evelynk04@users.noreply.github.com>
2025-11-17 14:27:22 -0500

    Update chatbot response to include WIFI support

--- chatbot responses.py
+++ chatbot responses.py
@@ -10,4 +10,5 @@ def get_response(user_input):
     elif "help" in user_input or "support" in user_input:
         return "I can help with MFA login, CUNYFirst, or password resets. What issue are you facing?"
     else:
-        return "Sorry, I didn’t understand that. Try asking about MFA, CUNYFirst, or password reset."
+        return "Sorry, I didn’t understand that. Try asking about MFA, CUNYFirst, WIFI or password reset."
+
